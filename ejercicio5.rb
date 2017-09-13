3494c95ea98b263c23b549d3a83cf9fa9f6b28b7
mycamach <manoli.camacho.a@gmail.com>
2017-09-13 20:38:07 -0300

    5 ready

--- ejercicio5.rb
+++ ejercicio5.rb
@@ -5,18 +5,10 @@ class Morseable
 
   def generate_hash(number)
     # Esto es una aberración y debe ser refactorizado!
-    h = '-----' if number == 0
-    h = '.----' if number == 1
-    h = '..---' if number == 2
-    h = '...--' if number == 3
-    h = '....-' if number == 4
-    h = '.....' if number == 5
-    h = '-....' if number == 6
-    h = '--...' if number == 7
-    h = '---..' if number == 8
-    h = '----.' if number == 9
+    h = {0 => '-----', 1 => '.----', 2 => '..---', 3 => '...--', 4 => '....-', 5 => '.....', 6 => '-....', 7 => '--...', 8 => '---..', 9 => '----.'}
+    puts h[number]
   end
-
+  
   def to_morse
     self.generate_hash(@number)
   end
